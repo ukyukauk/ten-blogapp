41ccf395fe958d4fd4089ac92ab7a3b1c4f59da7
yukten <yuk7829@gmail.com>
2025-04-23 20:22:20 +0900

    fakerでダミーデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,9 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Article.create({title: '新しい記事だよ', content: '素晴らしい記事です。'})
-Article.create({title: 'ああああああああ', content: 'いいいいいいいいいいいい'})
+10.times do
+  Article.create(
+    title: Faker::Lorem.sentence(word_count: 5),
+    content: Faker::Lorem.sentence(word_count: 100)
+  )
+end
