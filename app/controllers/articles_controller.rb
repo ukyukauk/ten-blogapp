7844da015b815c86236c51cfc5aac1616cdc8010
yukten <yuk7829@gmail.com>
2025-04-23 21:07:39 +0900

    createアクション

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,5 +12,16 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
