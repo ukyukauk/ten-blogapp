113b8bbc9d8207b5816a447b8114ff3fdb93a42f
yukten <yuk7829@gmail.com>
2025-04-26 21:00:03 +0900

    いいねをできるように＆いいねをしたらハートを赤色に

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 
   resources :articles do
     resources :comments, only: [:new, :create]
+
+    resource :like, only: [:create]
   end
 
   resource :profile, only: [:show, :edit, :update]
