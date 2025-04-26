82742a369c48b03f318a3a4d588e840acbc92c82
yukten <yuk7829@gmail.com>
2025-04-26 18:17:30 +0900

    プロフィールを更新できるように

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,6 +6,28 @@ class ProfilesController < ApplicationController
   end
 
   def edit
-    @profile = current_user.build_profile
+    @profile = current_user.prepare_profile
+  end
+
+  def update
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィールを更新しました'
+    else
+      flash.now[:error] = 'プロフィールの更新に失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :gender,
+      :birthday,
+      :subscribed
+    )
   end
 end
