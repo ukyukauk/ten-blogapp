e3e7668dcda6f7749a1793f8a23da10c56ed2a34
yukten <yuk7829@gmail.com>
2025-04-28 16:49:08 +0900

    contentカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_28_073407) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_28_074515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,7 +55,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_28_073407) do
   create_table "articles", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "title", null: false
-    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
