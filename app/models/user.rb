0f0899b59ca5c8e79290ae8bb02064b0bb31bed0
yukten <yuk7829@gmail.com>
2025-04-30 20:24:25 +0900

    アンフォロー機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,17 +50,13 @@ class User < ApplicationRecord
   end
 
   def follow!(user)
-    if user.is_a?(User)
-      user_id = user.id
-    else
-      user_id = user
-    end
-
+    user_id = get_user_id(user)
     following_relationships.create!(following_id: user_id)
   end
 
   def unfollow!(user)
-    relation = follower_relationships.find_by!(follower_id: user.id)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
     relation.destroy!
   end
 
@@ -78,6 +74,15 @@ class User < ApplicationRecord
       profile.avatar
     else
       'default-avatar.png'
+    end
+  end
+
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
     end
   end
 end
