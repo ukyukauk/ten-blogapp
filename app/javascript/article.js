2d69858431fdb8d67deb6a297521df8fc9768167
yukten <yuk7829@gmail.com>
2025-05-13 20:44:16 +0900

    JSのモジュール

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -1,8 +1,9 @@
 import $ from "jquery";
-import axios from "axios";
-import Rails from "@rails/ujs";
-
-axios.defaults.headers.common["X-CSRF-Token"] = Rails.csrfToken();
+import axios from "./modules/axios";
+import {
+  listenInactiveHeartEvent,
+  listenActiveHeartEvent
+} from './modules/handle_heart';
 
 const handleHeartDisplay = (hasLiked) => {
   if (hasLiked) {
@@ -64,31 +65,6 @@ document.addEventListener("turbo:load", () => {
       handleHeartDisplay(hasLiked);
   });
 
-  $(".inactive-heart").on("click", () => {
-    axios.post(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status === "ok") {
-          $(".active-heart").removeClass("hidden");
-          $(".inactive-heart").addClass("hidden");
-        }
-      })
-      .catch((e) => {
-        window.alert("Error");
-        console.log(e);
-      });
-  });
-
-  $(".active-heart").on("click", () => {
-    axios.delete(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status === "ok") {
-          $(".active-heart").addClass("hidden");
-          $(".inactive-heart").removeClass("hidden");
-        }
-      })
-      .catch((e) => {
-        window.alert("Error");
-        console.log(e);
-      });
-  });
+  listenInactiveHeartEvent(articleId);
+  listenActiveHeartEvent(articleId);
 });
